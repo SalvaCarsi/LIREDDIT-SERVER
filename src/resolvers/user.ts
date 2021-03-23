760fa230497a6a0358391ec625612b85f2876016
Salva Carsi Moreno <scarsi@mercadona.es>
2021-03-23 17:50:43 +0100

    use query builder for registering users

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import { User } from '../entities/User'
 import { MyContext } from '../../types'
 import argon2 from 'argon2'
+import { EntityManager } from '@mikro-orm/postgresql'
 
 @InputType()
 class UsernamePasswordInput {
@@ -66,25 +67,32 @@ export class UserResolver {
         ],
       }
     }
-    if (options.password.length <= 3) {
+    if (options.password.length <= 2) {
       return {
         errors: [
           {
             field: 'password',
-            message: 'length must be greater than 3',
+            message: 'length must be greater than 2',
           },
         ],
       }
     }
 
     const hashedPassword = await argon2.hash(options.password)
-    const user = await em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    })
+    let user
 
     try {
-      await em.persistAndFlush(user)
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*')
+        user = result[0]
     } catch (error) {
       if (error.code === '23505') {
         return {
